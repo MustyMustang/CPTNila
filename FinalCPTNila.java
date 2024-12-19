bf876d3577cbbf800ad21951b01a663f72a1532f
MustyMustang <nila.mathivannan26@ycdsbk12.ca>
2024-12-19 13:32:32 -0500

    Update FinalCPTNila.java

--- FinalCPTNila.java
+++ FinalCPTNila.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class FinalCPTNila{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console(1280, 720);
 		con.println("Last day of school!!!");
 	}
 }
